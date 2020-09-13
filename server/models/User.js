372e8e4081f277079c8672c7f4d203931c27ad62
Haiyang Hong <hong961127@gmail.com>
2020-09-13 04:25:07 -0700

    Add user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,47 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 12,
+      unique: true,
+      lowercase: true,
+      index: true, // Increase query performance
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
