ec9ffba8bb8a9fb5ec6ad1734aeb882db9c7a17e
Haiyang Hong <hong961127@gmail.com>
2020-09-13 04:17:33 -0700

    Setup mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
+const connectDB = require("./config/db");
 
 // Routers
 const authRouter = require("./routes/auth");
 
+connectDB();
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
