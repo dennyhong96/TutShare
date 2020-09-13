f213966ffd51580fcf2f0862a847e17922c75710
Haiyang Hong <hong961127@gmail.com>
2020-09-13 02:37:38 -0700

    Start on register form

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+import styles from "../styles/Auth.module.scss";
+
 const register = () => {
-  return <h1>Register</h1>;
+  return (
+    <div className={styles["register"]}>
+      <div className={styles["register__paper"]}>
+        <form className={styles["register__form"]}>
+          <div className={styles["register__form__control"]}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Enter your email" />
+          </div>
+          <div className={styles["register__form__control"]}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Password" />
+          </div>
+          <div className={styles["register__form__control"]}>
+            <label htmlFor="passwordConfirm">Confirm Password</label>
+            <input
+              type="password"
+              id="passwordConfirm"
+              placeholder="Confirm your password"
+            />
+          </div>
+          <button>Register</button>
+        </form>
+        <div className={styles["register__features"]}>Features</div>
+      </div>
+    </div>
+  );
 };
 
 export default register;
