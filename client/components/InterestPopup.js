edbcebd30c9c6ed6d3a3bbdf1e3d85def40cf4cb
Haiyang Hong <hong961127@gmail.com>
2020-09-20 21:28:30 -0700

    Add update interests popup

---
+++ client/components/InterestPopup.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "../utils/axios";
+import { API } from "../config";
+import Modal from "../components/Modal";
+import { useSelector } from "react-redux";
+import styles from "../styles/components/interestPopup.module.scss";
+
+const InterestPopup = () => {
+  const user = useSelector(({ user: { user } }) => user);
+
+  const [neverShowPopup, setNeverShowPopup] = useState(
+    localStorage.getItem("HIDE_INTEREST_POPUP")
+  );
+
+  const handleNeverShow = () => {
+    localStorage.setItem("HIDE_INTEREST_POPUP", "TRUE");
+    setNeverShowPopup("TRUE");
+  };
+
+  const [categories, setCategories] = useState(null);
+  const [categorySelection, setCategorySelection] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`${API}/v1/categories`);
+
+        setCategories(res.data.data.categories);
+        setCategorySelection(
+          res.data.data.categories.reduce((acc, cur) => {
+            acc[cur] = false;
+            return acc;
+          }, {})
+        );
+      } catch (error) {
+        console.error(error.response);
+      }
+    })();
+  }, []);
+
+  const handleCategoryChange = (evt) => {
+    const { id } = evt.target;
+    setCategorySelection((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
+
+  return (
+    <Modal show={!neverShowPopup && user && !user.interestedIn.length}>
+      <div className={styles["_wrapper"]}>
+        <h4>Please select tenologies you are interested in.</h4>
+        <div className={styles["_input-section"]}>
+          {categories &&
+            categorySelection &&
+            categories.map((cate) => (
+              <label
+                key={cate._id}
+                className={styles["_input-section__check"]}
+                htmlFor={cate._id}
+              >
+                <input
+                  type="checkbox"
+                  hidden
+                  checked={categorySelection[cate._id]}
+                  id={cate._id}
+                  onChange={handleCategoryChange}
+                />
+                <div className={styles["_input-section__check__icon"]}>
+                  {categorySelection[cate._id] ? (
+                    <i className="far fa-check-square"></i>
+                  ) : (
+                    <i className="far fa-square"></i>
+                  )}
+                </div>
+                <p className={styles["_input-section__check__name"]}>
+                  {cate.name}
+                </p>
+              </label>
+            ))}
+        </div>
+        <p>
+          You will receive email notifications when new resources of your
+          interest come in.
+        </p>
+        <small>
+          You can always update your selection from your account page.
+        </small>
+        <div className={styles["_actions"]}>
+          <button
+            className={styles["_actions__neverShow"]}
+            onClick={handleNeverShow}
+          >
+            Never show again
+          </button>
+          <button className={styles["_actions__confirm"]}>
+            Update interests
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default InterestPopup;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,22 +5,16 @@ import "react-quill/dist/quill.snow.css";
 
 import store from "../redux/store";
 import Layout from "../components/Layout";
+import InterestPopup from "../components/InterestPopup";
 import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ReduxProvider store={store}>
       <Layout>
-        {/* <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-            integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-            crossOrigin="anonymous"
-          />
-        </Head> */}
         <Component {...pageProps} />
       </Layout>
+      {process.browser && <InterestPopup />}
     </ReduxProvider>
   );
 };
